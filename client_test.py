05bb0a46efee13b18cc9dc22c6006922ae0a8b48
Saira7 <sairaalvi03@gmail.com>
2024-08-07 11:53:16 +0500

    added tests in client_test

--- client_test.py
+++ client_test.py
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+        self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +17,22 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
 
 
   """ ------------ Add more unit tests ------------ """
-
+  def test_normal_case(self):
+    self.assertEqual(getRatio(10, 2), 5)
+    
+  def test_zero_division(self):
+    self.assertIsNone(getRatio(10, 0))
+    
+  def test_negative_values(self):
+    self.assertEqual(getRatio(-10, 2), -5)
+    
+  def test_zero_values(self):
+    self.assertEqual(getRatio(0, 10), 0)
 
 
 if __name__ == '__main__':
